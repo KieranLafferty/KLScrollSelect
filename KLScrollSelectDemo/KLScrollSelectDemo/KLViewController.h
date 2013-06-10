9f3c0cbb4745ec6d020495fcfa8e74c57ca3e3f5
Kieran <kieranlafferty@Kierans-MacBook-Pro-2.local>
2013-06-09 23:35:38 -0400

    Programmatically instantiated Demo

--- KLScrollSelectDemo/KLScrollSelectDemo/KLViewController.h
+++ KLScrollSelectDemo/KLScrollSelectDemo/KLViewController.h
@@ -12,7 +12,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *facebookButton;
 @property (weak, nonatomic) IBOutlet UIButton *tweetButton;
-@property (weak, nonatomic) IBOutlet KLScrollSelect *scrollSelect;
+@property (strong, nonatomic) IBOutlet KLScrollSelect *scrollSelect;
 @property (nonatomic, strong) NSArray* leftColumnData;
 @property (nonatomic, strong) NSArray* rightColumnData;
 - (IBAction)didSelectTweetButton:(id)sender;
